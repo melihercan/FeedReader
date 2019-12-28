c8c6420a908911a09950e1d4122679f000da62aa
Melih Ercan <melihercan@gmail.com>
2019-12-28 16:02:18 +0100

    Added Ui and Backend components.

--- tests/WebUI.IntegrationTests/TestCurrentUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using FeedReader.Application.Common.Interfaces;
-
-namespace FeedReader.WebUI.IntegrationTests
-{
-    public class TestCurrentUserService : ICurrentUserService
-    {
-        public string UserId => "00000000-0000-0000-0000-000000000000";
-    }
-}
