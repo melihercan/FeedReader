c30eca37246671c009ef5b23e3a880f9b24ae177
Melih Ercan <melihercan@gmail.com>
2019-12-29 14:25:12 +0100

    Added CreateFeed command.

---
+++ Core/Application/Feed/Commands/CreateFeed/CreateFeedCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Feed.Commands.CreateFeed
+{
+    public class CreateFeedCommand : IRequest
+    {
+        public string FeedUrl { get; set; }
+
+    }
+}
---
+++ Core/Application/Feed/Commands/CreateFeed/CreateFeedCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Feed.Commands.CreateFeed
+{
+    public class CreateFeedCommandHandler : IRequestHandler<CreateFeedCommand>
+    {
+        public Task<Unit> Handle(CreateFeedCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
