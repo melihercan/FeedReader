33121fe8b457efa1770ffdeff6cea24871607960
Melih Ercan <melihercan@gmail.com>
2020-01-12 15:16:21 +0300

    Added user interfaces.

---
+++ UserInterfaces/ConsoleUi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
