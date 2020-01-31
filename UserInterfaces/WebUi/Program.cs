0ff3698a5754662bbb8f871c0860ccd72cf53c20
Melih Ercan <melihercan@gmail.com>
2020-01-31 15:37:05 +0300

    Using new version of Blazor.

--- UserInterfaces/WebUi/Program.cs
+++ UserInterfaces/WebUi/Program.cs
@@ -1,16 +1,31 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using Interactors.Interfaces;
+using Interactors.Feed.Commands.CreateFeed;
+using Gateways.Identity;
+using System.Reflection;
 
 namespace WebUi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services.AddMediatR(typeof(CreateFeedCommand).GetTypeInfo().Assembly);
+
+            builder.Services.AddSingleton<IFeedRepository, Gateways.FeedRepository.Lib.FeedRepository>();
+            builder.Services.AddSingleton<IIdentity, Identity>();
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
     }
 }
