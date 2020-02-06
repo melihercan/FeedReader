358145076fc24ee18cf68dd596895d12557bd2e2
Melih Ercan <melihercan@gmail.com>
2020-02-06 16:16:45 +0300

    Removed Blazorise library. I will use Bootstrap only.

--- UserInterfaces/WebUi/Program.cs
+++ UserInterfaces/WebUi/Program.cs
@@ -9,9 +9,6 @@ using Interactors.Interfaces;
 using Interactors.Feed.Commands.CreateFeed;
 using Gateways.Identity;
 using System.Reflection;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
 
 namespace WebUi
 {
@@ -20,31 +17,13 @@ namespace WebUi
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
             builder.Services.AddMediatR(typeof(CreateFeedCommand).GetTypeInfo().Assembly);
 
             builder.Services.AddSingleton<IFeedRepository, Gateways.FeedRepository.Lib.FeedRepository>();
             builder.Services.AddSingleton<IIdentity, Identity>();
-
-
-            builder.Services
-                .AddBlazorise(options =>
-                {
-                    options.ChangeTextOnKeyPress = true;
-                })
-                .AddBootstrapProviders()
-                .AddFontAwesomeIcons();
-
             builder.RootComponents.Add<App>("app");
 
-            var host = builder.Build();
-            host.Services
-                .UseBootstrapProviders()
-                .UseFontAwesomeIcons();
-
-
-
-            await host.RunAsync();
+            await builder.Build().RunAsync();
         }
     }
 }
