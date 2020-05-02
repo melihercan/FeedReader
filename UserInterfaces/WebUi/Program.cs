ff153c719d7699abf7cd3e4e48653ceb121d46d4
Melih Ercan <melihercan@gmail.com>
2020-05-02 13:04:44 +0300

    WebUi building OK.

--- UserInterfaces/WebUi/Program.cs
+++ UserInterfaces/WebUi/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace WebUi.Client
+namespace WebUi
 {
     public class Program
     {
