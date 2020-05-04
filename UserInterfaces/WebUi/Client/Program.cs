468c04d03402b7529e68bd60923b844c31d156ab
Melih Ercan <melihercan@gmail.com>
2020-05-04 20:38:21 +0300

    Added WebUi client and server.

--- UserInterfaces/WebUi/Client/Program.cs
+++ UserInterfaces/WebUi/Client/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace WebUi
+namespace WebUi.Client
 {
     public class Program
     {
