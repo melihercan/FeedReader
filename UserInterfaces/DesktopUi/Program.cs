ab6c8ad8cb14e0dfc3eb930885c925a36ed0047f
Melih Ercan <melihercan@gmail.com>
2020-05-29 13:41:59 +0300

    DesktopUi project is ready. Need to add authentication and other services later.

--- UserInterfaces/DesktopUi/Program.cs
+++ UserInterfaces/DesktopUi/Program.cs
@@ -1,13 +1,5 @@
+using WebWindows.Blazor;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DesktopUi
 {
@@ -15,14 +7,8 @@ namespace DesktopUi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ComponentsDesktop.Run<Startup>("FeedReader", "wwwroot/index.html");
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
