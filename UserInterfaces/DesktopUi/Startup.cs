bea2b8dd155edf30966fd0a440abcaf9e916869a
Melih Ercan <melihercan@gmail.com>
2020-07-23 11:31:35 +0300

    I will add DesktopUi from blazor server and electron.

--- UserInterfaces/DesktopUi/Startup.cs
+++ UserInterfaces/DesktopUi/Startup.cs
@@ -1,42 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Application;
 using Infrastructure;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Shared;
-using System;
-using System.Net.Http;
 using WebUi;
-using WebWindows.Blazor;
-
 
 namespace DesktopUi
 {
     public class Startup
     {
-        private const string _webApiName = "Infrastructure.ServerAPI";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-#pragma warning disable ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
-            var configuration = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
-#pragma warning restore ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
-            services.AddHttpClient(_webApiName, client => client.BaseAddress =
-                new Uri(configuration["Server:URL"]));
-            services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>()
-                .CreateClient(_webApiName));
-
-            ////services.AddApiAuthorization();
-
-
-            services.AddAuthorizationCore();
-            services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
-
-            services.AddSingleton<SignOutSessionStateManager>();
-
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
 
             services.AddWebUiServices();
             services.AddUser();
@@ -48,13 +41,30 @@ namespace DesktopUi
             services.AddApplication();
         }
 
-
-
-        public void Configure(DesktopApplicationBuilder app)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            Registry.ServiceProvider = app.Services;
-            app.AddComponent<App>("app");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
         }
     }
 }
