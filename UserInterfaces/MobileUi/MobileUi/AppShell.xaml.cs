998f8576c0833013e8312d2cfd46fa8e701dffe7
Melih Ercan <melihercan@gmail.com>
2020-09-29 16:19:24 +0300

    About page.

--- UserInterfaces/MobileUi/MobileUi/AppShell.xaml.cs
+++ UserInterfaces/MobileUi/MobileUi/AppShell.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using MobileUi.Views;
 using System;
 using System.Collections.Generic;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace MobileUi
@@ -14,8 +14,20 @@ namespace MobileUi
 
             Routing.RegisterRoute("register", typeof(RegisterPage));
             Routing.RegisterRoute("feedchannel", typeof(FeedChannelPage));
+            Routing.RegisterRoute("about", typeof(AboutPage));
 
             BindingContext = this;
         }
+
+        public ICommand LogoutCommand => new Command(async () =>
+        {
+        });
+
+        public ICommand AboutCommand => new Command(async () =>
+        {
+            Shell.Current.FlyoutIsPresented = false;
+            await Shell.Current.GoToAsync("about");
+        });
+
     }
 }
