2102de97dee014b92918f4eb1841c0a00a038808
Melih Ercan <melihercan@gmail.com>
2020-09-29 19:15:37 +0300

    Logout is working.

--- UserInterfaces/MobileUi/MobileUi/AppShell.xaml.cs
+++ UserInterfaces/MobileUi/MobileUi/AppShell.xaml.cs
@@ -1,15 +1,25 @@
-﻿using MobileUi.Views;
+﻿using MediatR;
+using MobileUi.Views;
+using Shared;
 using System;
 using System.Collections.Generic;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using Application.UseCases;
+using Ardalis.Result;
+using System.Text.Json;
 
 namespace MobileUi
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
+        private readonly IMediator _mediator;
+
         public AppShell()
         {
+            _mediator = Registry.ServiceProvider.GetService<IMediator>();
+
             InitializeComponent();
 
             Routing.RegisterRoute("register", typeof(RegisterPage));
@@ -21,6 +31,16 @@ namespace MobileUi
 
         public ICommand LogoutCommand => new Command(async () =>
         {
+            var result = await _mediator.Send(new Logout());
+            if (result.Status == ResultStatus.Ok)
+            {
+                var schemesResult = await _mediator.Send(new GetAuthenticationSchemes());
+                if (schemesResult.Status == ResultStatus.Ok)
+                {
+                    var schemesJson = JsonSerializer.Serialize(schemesResult.Value);
+                    await Shell.Current.GoToAsync($"///login?schemesjson={schemesJson}");
+                }
+            }
         });
 
         public ICommand AboutCommand => new Command(async () =>
